ef5080721043eb9b305e8ea83b1a80b509aa5bb9
Pedro Goncalves <pgoncalves.pt@gmail.com>
2021-04-05 20:36:02 +0200

    commit 4

--- scripts/reynoldsCalc_9-1.py
+++ scripts/reynoldsCalc_9-1.py
@@ -7,16 +7,13 @@ d=150/1000 #diameter, m
 v=5.5 #velocity,m/s
 
 #get properties
-
 water=Chemical('water', T=t_water, P=1E5)
 rho_water=water.rho #density
 print('water density',rho_water)
 mu_water=water.mu #viscosity dynamic
 print('water dynamic viscosity', mu_water)
 
-
 # print(mu_water/rho_water)
-
 n_reynolds=rho_water*v*d/mu_water
 print('renolds number=',n_reynolds)
 
